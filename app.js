c654707ac7cb45d713b33ae2015bc78d3e450b28
GallaPriyanka <priyankagalla96@gmail.com>
2019-03-08 16:50:06 -0600

    added console.log for each app.get

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 const express = require('express')
 const app = express()
 
-const hostname = '127.0.0.1'   // set constants
+const hostname = '0.0.0.0'   // allows remote computers to access
 const port = 3002
 
 app.get('/', function (req, res) {
+  console.log('Get request for /')
   res.send('Welcome home!')
 })
 
 app.get('/hello', (req, res) => {
+  console.log('Get request for /hello')
   res.send('Hello World!')
 })
 
 app.get('/big',  (req, res) =>{
+  console.log('Get request for /big')
   res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/greeting/:id',  (req, res) =>{
+  console.log('Get request for /greeting/:id')
   res.send('Hello! The id was ' + req.params.id)
 })
 
 app.get('/yo/:buddy',  (req, res) =>{
+  console.log('Get request for /yo/:buddy')
   res.send('<h1>Yo, ' + req.params.buddy + '!</h1>')
 })
 
